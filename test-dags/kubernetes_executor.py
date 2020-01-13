f432ad9b7cca60274fdab8a7ab2db24d4e2991a9
Avishek Das <avishek.akd@gmail.com>
2020-01-13 22:23:03 +0530

    j

--- test-dags/kubernetes_executor.py
+++ test-dags/kubernetes_executor.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
 
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -17,30 +18,29 @@ default_args = {
 dag = DAG(
     'kubernetes_sample', default_args=default_args, schedule_interval=timedelta(minutes=10))
 
+
 start = DummyOperator(task_id='run_this_first', dag=dag)
 
-passing = KubernetesPodOperator(namespace='airflow',
-                          image="python:3.6",
-                          cmds=["python","-c"],
+passing = KubernetesPodOperator(namespace='default',
+                          image="Python:3.6",
+                          cmds=["Python","-c"],
                           arguments=["print('hello world')"],
                           labels={"foo": "bar"},
                           name="passing-test",
                           task_id="passing-task",
                           get_logs=True,
-                          dag=dag,
-                          in_cluster=True
+                          dag=dag
                           )
 
-failing = KubernetesPodOperator(namespace='airflow',
-                          image="ubuntu:16.04",
-                          cmds=["python","-c"],
+failing = KubernetesPodOperator(namespace='default',
+                          image="ubuntu:1604",
+                          cmds=["Python","-c"],
                           arguments=["print('hello world')"],
                           labels={"foo": "bar"},
                           name="fail",
                           task_id="failing-task",
                           get_logs=True,
-                          dag=dag,
-                          in_cluster=True
+                          dag=dag
                           )
 
 passing.set_upstream(start)

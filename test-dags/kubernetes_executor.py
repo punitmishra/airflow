b140a2bdfba1b20c3b300563cb386fc25c4d5faa
Avishek Das <avishek.akd@gmail.com>
2020-01-13 22:54:46 +0530

    j

--- test-dags/kubernetes_executor.py
+++ test-dags/kubernetes_executor.py
@@ -2,21 +2,20 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.dates import days_ago
 
 
 default_args = {
     'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime.utcnow(),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
+    'start_date': days_ago(2),
 }
 
 dag = DAG(
-    'kubernetes_sample', default_args=default_args, schedule_interval=timedelta(minutes=10))
+    dag_id='kubernetes_sample',
+    default_args=default_args,
+    schedule_interval='0 0 * * *',
+    dagrun_timeout=timedelta(minutes=60)
+)
 
 
 start = DummyOperator(task_id='run_this_first', dag=dag)

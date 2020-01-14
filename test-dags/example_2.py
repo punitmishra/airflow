948ae49de375a96574dfe9792cc65ef091f3b9dd
Avishek Das <avishek.akd@gmail.com>
2020-01-14 23:19:42 +0530

    dnfkdnf

---
+++ test-dags/example_2.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.dates import days_ago
+
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(2),
+}
+
+dag = DAG(
+    dag_id='kubernetes_sample',
+    default_args=default_args,
+    schedule_interval='0 0 * * *',
+    dagrun_timeout=timedelta(minutes=60)
+)
+
+
+start = DummyOperator(task_id='run_this_first', dag=dag)
+
+passing = KubernetesPodOperator(namespace='default',
+                          image="Python:3.6",
+                          cmds=["Python","-c"],
+                          arguments=["print('hello world')"],
+                          labels={"foo": "bar"},
+                          name="passing-test",
+                          task_id="passing-task",
+                          get_logs=False,
+                          dag=dag
+                          )
+
+
+
+passing.set_upstream(start)
+

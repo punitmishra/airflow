e571ad7c808654ff34c085f1a310e4987dac4db4
Avishek Das <avishek.akd@gmail.com>
2020-01-22 01:00:50 +0530

    f

--- test-dags/test.py
+++ test-dags/test.py
@@ -14,44 +14,9 @@ with models.DAG(
         task_id='t1',
         name='task1',
         namespace='default',
-        image='eu.gcr.io/taiyo-239217/dag:fae4886',
+        image='eu.gcr.io/taiyo-239217/dag:fae4887',
         arguments=["AlphaVantage()"],
         in_cluster=True,
         xcom_push=True,
         is_delete_operator_pod=True
     )
-
-    task2 = kubernetes_pod_operator.KubernetesPodOperator(
-        task_id='t2',
-        name='task2',
-        namespace='default',
-        image='eu.gcr.io/taiyo-239217/dag:fae4886',
-        arguments=["FRED()"],
-        in_cluster=True,
-        xcom_push=True,
-        is_delete_operator_pod=True
-    )
-
-    task3 = kubernetes_pod_operator.KubernetesPodOperator(
-        task_id='t3',
-        name='task3',
-        namespace='default',
-        image='eu.gcr.io/taiyo-239217/dag:fae4886',
-        arguments=["TechFeatures()"],
-        in_cluster=True,
-        xcom_push=True,
-        is_delete_operator_pod=True
-    )
-
-    task4 = kubernetes_pod_operator.KubernetesPodOperator(
-        task_id='t4',
-        name='task4',
-        namespace='default',
-        image='eu.gcr.io/taiyo-239217/dag:fae4886',
-        arguments=["DataAggregation()"],
-        in_cluster=True,
-        xcom_push=True,
-        is_delete_operator_pod=True
-    )
-
-[task1, task2] >> task3 >> task4

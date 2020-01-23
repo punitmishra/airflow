a5b991dfda06fd53423bf649cbeac36a0ac30eec
Avishek Das <avishek.akd@gmail.com>
2020-01-23 12:49:41 +0530

    f

--- test-dags/test.py
+++ test-dags/test.py
@@ -5,6 +5,18 @@ from airflow.operators import dummy_operator
 
 YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
 
+volume_mount = VolumeMount('test-volume',
+                            mount_path='/root/mount_file',
+                            sub_path=None,
+                            read_only=True)
+volume_config= {
+    'persistentVolumeClaim':
+      {
+        'claimName': 'test-volume'
+      }
+    }
+volume = Volume(name='test-volume', configs=volume_config)
+
 with models.DAG(
         dag_id='demo',
         schedule_interval=datetime.timedelta(days=1),
@@ -16,6 +28,8 @@ with models.DAG(
         namespace='default',
         image='eu.gcr.io/taiyo-239217/dag:fae4887',
         arguments=["AlphaVantage()"],
+		volume=[volume],
+        volume_mounts=[volume_mount],
         in_cluster=True,
         xcom_push=True,
         is_delete_operator_pod=True
